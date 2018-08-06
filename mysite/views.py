b403d99a3777b49783d9ddb49973887db81f57e8
Amariswade <1247505666@qq.com>
2018-08-06 21:49:53 +0800

    cwd

--- mysite/views.py
+++ mysite/views.py
@@ -5,7 +5,7 @@ from .models import Post
 def homepage(request):
     posts = Post.objects.all()
     post_list = []
-    for count,post in enumerate(posts):
+    for count,post in enumerate(posts,1):
         post_list.append("No.{}：<b>{}</b><hr>".format(count,post.title))
         post_list.append(post.body+"<br>")
     return HttpResponse(post_list)
